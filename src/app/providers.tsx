dab4d04b6fc9333d51616189dcd288d6b1b999df
szeedneigh <sidneyjohn1704@gmail.com>
2025-11-10 23:33:00 +0800

    chore: updates

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,6 +2,8 @@
 
 import { Toaster } from '@/components/ui/sonner'
 import { ThemeProvider } from 'next-themes'
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar'
+import { NavigationLoading } from '@/components/shared/navigation-loading'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -11,6 +13,17 @@ export function Providers({ children }: { children: React.ReactNode }) {
       enableSystem
       disableTransitionOnChange
     >
+      {/* Top Progress Bar for Navigation */}
+      <ProgressBar
+        height="3px"
+        color="#0693D2"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
+      
+      {/* Loading Overlay for Slow Navigation (>300ms) */}
+      <NavigationLoading />
+      
       {children}
       <Toaster richColors position="top-right" />
     </ThemeProvider>
