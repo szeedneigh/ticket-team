56b9ae3729e5fa06ec2fd458f66c4f0cd6ae3b46
szeedneigh <sidneyjohn1704@gmail.com>
2025-11-19 23:49:24 +0800

    feat(settings): add settings

---
+++ src/components/settings/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Settings Components
+ *
+ * Export all settings-related components
+ */
+
+export { SystemConfigSettings } from './system-config-settings'
+export { EmailSettings } from './email-settings'
+export { DepartmentSettings } from './department-settings'
+export { CategorySettings } from './category-settings'
