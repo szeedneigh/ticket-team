6f984fa63d90201f39ecc361f06e2f7e30e01fcf
szeedneigh <sidneyjohn1704@gmail.com>
2025-11-28 23:40:49 +0800

    chore: updates

--- src/components/tickets/comment-item.tsx
+++ src/components/tickets/comment-item.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { formatDistanceToNow } from 'date-fns'
 import { Lock, Paperclip } from 'lucide-react'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
