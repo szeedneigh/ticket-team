6ae1b07e7779400f6e9504bce926f248391a9d3a
szeedneigh <sidneyjohn1704@gmail.com>
2025-12-19 13:51:22 +0800

    chore: updates and fixes

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -14,7 +14,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
-import { Search, PanelLeft } from 'lucide-react'
+import { PanelLeft } from 'lucide-react'
 import { NotificationBell } from '@/components/notifications/notification-bell'
 import type { User } from '@/lib/types/users'
 
