42e8e39a2c658dbea39ad11f0be5b3ea792efbdd
szeedneigh <sidneyjohn1704@gmail.com>
2025-12-19 12:03:28 +0800

    chore: updates and fixes

--- src/components/users/reactivate-user-dialog.tsx
+++ src/components/users/reactivate-user-dialog.tsx
@@ -59,7 +59,7 @@ export function ReactivateUserDialog({
           variant: 'destructive',
         })
       }
-    } catch (error) {
+    } catch {
       toast({
         title: 'Error',
         description: 'An unexpected error occurred',
